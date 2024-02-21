eda99408f476dc3734cde255ef73098c9b2ae888
PHAM NAM PHUONG <114424679+Mal3uz@users.noreply.github.com>
2024-02-22 00:45:11 +0700

    fix DBContext.java

--- src/java/dao/DBContext.java
+++ src/java/dao/DBContext.java
@@ -1,38 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  *
  * @author DELL
  */
 public class DBContext {
-    public Connection getConnection() throws SQLException {
-        try {
-            Connection con = null;
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=happyjob;encrypt=true;trustServerCertificate=true; user = sa; password = Tuna1.dev;";
-            con = DriverManager.getConnection(url);
-            return con;
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return null;
+      /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+     public Connection getConnection()throws Exception {        
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber +
+                ";databaseName="+dbName;//+"; integratedSecurity=true";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");         
+        return DriverManager.getConnection(url, userID, password);
+//        return DriverManager.getConnection(url);
     }
-
+    
+    /*Insert your other code right after this comment*/
+   
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "localhost";
+    private final String dbName = "happyJob";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "123";
+    
     public static void main(String[] args) {
         try {
-            System.out.println(new DBContext().getConnection());
-            System.out.println("Success");
+            System.out.println(new DBContext().getConnection() );
         } catch (Exception e) {
-            System.out.println("Failed");
         }
     }
-}
-
+    
